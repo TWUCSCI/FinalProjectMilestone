28ff8b1914783a573dc9a3d3aa75f72c0dd275c7
bbaral1 <bbaral1@twu.edu>
2021-04-14 20:19:10 -0500

    Initial Commit

---
+++ FinalProject/EmployeeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmployeeViewController.swift
+//  FinalProject
+//
+//  Created by Barsha Baral on 4/14/21.
+//
+
+import UIKit
+
+class EmployeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Employee Screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalProject/GuestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GuestViewController.swift
+//  FinalProject
+//
+//  Created by Barsha Baral on 4/14/21.
+//
+
+import UIKit
+
+class GuestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Guest Screen"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
